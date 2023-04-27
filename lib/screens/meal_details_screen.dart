eddb8053686ea436dbbf2a5d9011e3265c866775
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-04-28 01:24:54 +0530

    chor: item details ingredients list

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:meal_app/data/dummy_data.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   static const routeName = '/meal-details';
@@ -8,15 +9,59 @@ class MealDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mealId =
+    final routeArgs =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
-
+    final mealId = routeArgs['id'];
+    final selectedMeal = dummyMeals.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(mealId['id'] as String),
+        title: Text(selectedMeal.title),
       ),
       body: Center(
-        child: Text('The meal item - ${mealId['id']}'),
+        child: Column(children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.green),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (context, index) => Card(
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    selectedMeal.ingredients[index],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          )
+        ]),
       ),
     );
   }
