96124be48c05dbbf03e37232812d5ef7496e10f0
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-05-04 06:57:26 +0530

    chor: unused import removal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/categoreies_scren.dart';
+
 import 'package:meal_app/screens/category_meals_screen.dart';
 import 'package:meal_app/screens/filter_screen.dart';
 
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (context) => const CategoryMealsScreen(),
         MealDetailsScreen.routeName: (context) => const MealDetailsScreen(),
         TabsScreen.routeName: (context) => const TabsScreen(),
-        FilterScreen.routeName:(context) => const FilterScreen()
+        FilterScreen.routeName: (context) => const FilterScreen()
       },
       // this acts as a 404 page standerd in flutter
       onUnknownRoute: (settings) => MaterialPageRoute(
